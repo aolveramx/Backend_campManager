e68bfcc92d2cddee5480c63075ffdb95cd7ff221
Ari Olvera <badillo.ari@gmail.com>
2021-07-07 16:18:12 -0500

    refactor(middlewares): error & async feat(Camp): slugify

--- controllers/camps.js
+++ controllers/camps.js
@@ -1,3 +1,5 @@
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 const Camp = require('../models/Camp')
 
 /**
@@ -5,33 +7,27 @@ const Camp = require('../models/Camp')
  * @desc    Get all camps
  * @access  Public
  */
-exports.getCamps = async (req, res, next) => {
-  try {
-    const camps = await Camp.find()
-    res.status(200).json({ success: true, count: camps.length, data: camps })
-  } catch (error) {
-    res.status(400).json({ success: false })
-  }
-} 
+exports.getCamps = asyncHandler(async (req, res, next) => {
+  const camps = await Camp.find()
+  res.status(200).json({ success: true, count: camps.length, data: camps })
+}) 
 
 /**
  * @route   GET api/v1/camps/:id
  * @desc    Get single camp
  * @access  Public
  */
-exports.getCamp = async (req, res, next) => {
-  try {
-    const camp = await Camp.findById(req.params.id)
-    
-    if (!camp) {
-      return res.status(400).json({ success: false })
-    }
-
-    res.status(200).json({ success: true, data: camp })
-  } catch (error) {
-    res.status(400).json({ success: false })
+exports.getCamp = asyncHandler(async (req, res, next) => {
+  const camp = await Camp.findById(req.params.id)
+  
+  if (!camp) {
+    return next(
+      new ErrorResponse(`Camp not found with id of ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({ success: true, data: camp })
+})
 
 /**
  * @route   POST api/v1/camps
@@ -39,14 +35,10 @@ exports.getCamp = async (req, res, next) => {
  * @access  Private
  * @role    admin
  */
-exports.createCamp = async (req, res, next) => {
-  try {
-    const camp = await Camp.create(req.body)
-    res.status(201).json({ success:true, data: camp })
-  } catch (error) {
-    res.status(400).json({ success: false })
-  }
-}
+exports.createCamp = asyncHandler(async (req, res, next) => {
+  const camp = await Camp.create(req.body)
+  res.status(201).json({ success:true, data: camp })
+})
 
 /**
  * @route   UPDATE api/v1/camps/:id
@@ -54,22 +46,20 @@ exports.createCamp = async (req, res, next) => {
  * @access  Private
  * @role    admin
  */
-exports.updateCamp = async (req, res, next) => {
-  try {
-    const camp = await Camp.findByIdAndUpdate(req.params.id, req.body, { 
-      new: true,
-      runValidators: true, 
-    })
+exports.updateCamp = asyncHandler(async (req, res, next) => {
+  const camp = await Camp.findByIdAndUpdate(req.params.id, req.body, { 
+    new: true,
+    runValidators: true, 
+  })
 
-    if (!camp) {
-      return res.status(400).json({ success: false })
-    }
-
-    res.status(200).json({ success: true, data: camp })
-  } catch (error) {
-    res.status(400).json({ success: false })
+  if (!camp) {
+    return next(
+      new ErrorResponse(`Camp not found with id of ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({ success: true, data: camp })
+})
 
 /**
  * @route   Delete api/v1/camps/:id
@@ -77,16 +67,14 @@ exports.updateCamp = async (req, res, next) => {
  * @access  Private
  * @role    admin
  */
-exports.deleteCamp = async (req, res, next) => {
-  try {
-    const camp = await Camp.findByIdAndDelete(req.params.id)
+exports.deleteCamp = asyncHandler(async (req, res, next) => {
+  const camp = await Camp.findByIdAndDelete(req.params.id)
 
-    if (!camp) {
-      return res.status(400).json({ success: false, data: {} })
-    }
-
-    res.status(200).json({ success: true })
-  } catch (error) {
-    res.status(400).json({ success: false })
+  if (!camp) {
+    return next(
+      new ErrorResponse(`Camp not found with id of ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({ success: true })
+})
