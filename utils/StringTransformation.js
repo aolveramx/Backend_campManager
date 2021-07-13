46bfc15877f06fc34b90270a4b30f1889ea1d3bb
Jose Antonio Perez <joseprzgonzalez@gmail.com>
2021-07-13 08:39:58 +0200

    string transformation - filtering

--- utils/StringTransformation.js
+++ utils/StringTransformation.js
@@ -3,6 +3,10 @@ let capitalizeFirstLetter = function(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
+let hasBlanckSpace = function(string) {
+    string = string.trim();
+}
+
 let queryCapitalized = function(searchObject) {
     if(searchObject.location) {
         searchObject.location=capitalizeFirstLetter(searchObject.location);
